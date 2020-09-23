a2f854d73036a8e48e974a36497cedaa720e348b
Subash Chinni (INFOSYS LIMITED) <v-subasc@microsoft.com>
2020-09-23 13:55:04 +0530

    amazonUI commit

--- src/webApps/AmazonUI/Body.tsx
+++ src/webApps/AmazonUI/Body.tsx
@@ -1,10 +1,36 @@
 import React from "react";
 import "./Body.scss";
+import styled from "styled-components";
+import amazon from "../../amazon.png";
+
+const Div = styled.div`
+  width: 1.2rem;
+  height: 2px;
+  background-color: #fff;
+  border-radius: 10px;
+  margin: 7px;
+  transform-origin: 1px;
+  transition: all 0.3s linear;
+`;
 
 export function AmazonHome() {
   return (
     <>
-      <div className="wrapper">Amazon UI</div>
+      <div className="wrapper">
+        <div className="menu-border">
+          <div>
+            <Div />
+            <Div />
+            <Div />
+          </div>
+        </div>
+        <img src={amazon} width="82" height="40" />
+        <div>
+          <button>All</button>
+          <input type="text" />
+          <button>S</button>
+        </div>
+      </div>
     </>
   );
 }
